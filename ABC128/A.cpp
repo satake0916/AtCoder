f71e5c014aaee8d3f37721f7eb6eb46de9c8bbfe
satake0916 <38389682+satake0916@users.noreply.github.com>
2019-05-26 23:46:51 +0900

    Create A.cpp

---
+++ ABC128/A.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+int INF = numeric_limits<int>::max() / 2;
+ll LINF = numeric_limits<ll>::max() / 2;
+int MOD = 1e9 + 7;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << (b + a * 3) / 2 << endl;
+}
