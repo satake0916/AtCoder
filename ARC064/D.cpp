0eaa1ef7697f4c93482df2f5351baf182608646d
satake0916 <38389682+satake0916@users.noreply.github.com>
2019-06-06 01:37:30 +0900

    Create D.cpp

---
+++ ARC064/D.cpp
@@ -0,0 +1,7 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+    string s; cin >> s;
+    cout << (!((s.size()%2 == 0) ^ (s[0] == s[s.size()-1])) ? "First" : "Second") << endl;
+}
