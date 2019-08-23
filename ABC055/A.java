6340d32f5c0bb7c3d3583b7178f59feb77ffcd11
satake0916 <38389682+satake0916@users.noreply.github.com>
2019-08-23 10:32:50 +0900

    Create A.java

---
+++ ABC055/A.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main{
+	public static void main(String[] $){
+		//入力
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+
+		//処理
+
+		//出力
+		System.out.println(800 * n - 200 * (n/15));
+	}
+}
