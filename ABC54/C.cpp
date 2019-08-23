2fb73e5a0387dcbfdc48997154be7b020fe0183e
satake0916 <38389682+satake0916@users.noreply.github.com>
2019-08-23 12:18:18 +0900

    Create C.cpp

---
+++ ABC54/C.cpp
@@ -0,0 +1,54 @@
+//g++でビルド
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int n, m;
+vector<int> fronts, backs;
+
+int dfs(vector<bool> bools, int s){
+    bools[s-1] = true;
+    bool last = true;
+    for(int i = 0; i < n; i++){
+        last = last && bools[i];
+    }
+    if(last){
+        return 1;
+    }
+    set<int> next;
+    for(int i = 0; i < fronts.size(); i++){
+        if(fronts[i] == s){
+            int back = backs[i];
+            if(!bools[back-1]){
+                next.insert(back);
+            }
+        }
+    }
+
+    int sum = 0;
+    for(int item : next){
+        sum += dfs(bools, item);
+    }
+
+    return sum;
+
+}
+
+int main(){
+    cin >> n >> m;
+    for(int i = 0; i<m; i++){
+        int a, b;
+        cin >> a >> b;
+        fronts.push_back(a);
+        backs.push_back(b);
+        fronts.push_back(b);
+        backs.push_back(a);
+    }
+    vector<bool> bools(n, false);
+    int result = dfs(bools, 1);
+    
+
+    cout << result << endl;
+    return 0;
+}
