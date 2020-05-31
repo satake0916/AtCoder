68fdbd42bdb66b2099b0127bd2cdb1f70524056e
satake0916 <38389682+satake0916@users.noreply.github.com>
2020-06-01 03:21:25 +0900

    Create E.cpp

---
+++ ABC167/E.cpp
@@ -0,0 +1,45 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+using ll = long long;
+
+const int MAX = 510000;
+const int MOD = 998244353;
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+int main(){
+    int n,m,k;
+    cin >> n >> m >> k;
+    COMinit();
+    int ans = 0;
+    vector<ll> pw(n, -1);
+    pw[0] = 1;
+    for(int i = 1; i < n; i++){
+        pw[i] = pw[i-1] * (m-1) % MOD;
+    }
+    for(int i = 0; i <= k; i++){
+        ll temp = m * COM(n-1, i) % MOD * pw[n-1-i] % MOD;
+        ans = (ans + temp) % MOD;
+    }
+    cout << ans << endl;
+}
