6e9a0c67ff270d5145b737bea3a25d94d0e9063b
satake0916 <38389682+satake0916@users.noreply.github.com>
2021-01-09 16:15:30 +0900

    Create D.cpp

---
+++ ABC187/D.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<long long> v;
+    long long def = 0;
+    for(int i = 0; i < n; i++){
+        long long a,b;
+        cin >> a >> b;
+        def -= a;
+        v.push_back(2*a + b);
+    }
+    sort(v.begin(), v.end());
+    int ans = 0;
+    for(int i = n-1; i >= 0; i--){
+        def += v[i];
+        ans++;
+        if(def > 0){
+            break;
+        }
+    }
+
+    cout << ans << endl;
+}
