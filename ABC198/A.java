f3dae19f388b19071b6632d88a951ff4b93c710a
satake0916 <38389682+satake0916@users.noreply.github.com>
2021-04-13 19:04:05 +0900

    Create A.java

---
+++ ABC198/A.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);	
+		int n = sc.nextInt();
+		sc.close();
+
+		System.out.println(n-1);
+	}
+}
