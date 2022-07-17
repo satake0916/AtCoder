6becd650af9641649b8dea6f2688ccb0d7c25081
satake makoto <stk_halfmoon@icloud.com>
2022-07-17 20:37:29 +0900

    Aのみ

---
+++ ABC251/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+	string s;
+	cin >> s;
+	for(int i = 0; i < 6 / s.size(); i++){
+		cout << s;
+	}
+	cout << endl;
+}
