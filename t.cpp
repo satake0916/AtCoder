64965fc5a4b9b6a46cde3194142c13cb343611d7
satake <stk_halfmoon@icloud.com>
2022-09-10 17:42:28 +0900

    debug.hを追加

--- t.cpp
+++ t.cpp
@@ -3,23 +3,12 @@ using namespace std;
 using ll = long long;
 
 #ifdef LOCAL
-#define debug(arg) print(#arg, arg)
-
-template <class Tp> void print(string_view name, Tp arg) {
-  cerr << "(debug) " <<  name << " = " << arg << endl;
-}
-
-template <class Tp> void print(string_view name, vector<Tp> arg) {
-  cerr << "(debug) " << name << " = [ ";
-  copy(cbegin(arg), cend(arg), ostream_iterator<Tp>(cerr, " "));
-  cerr << "]" << endl;;
-}
-
+#include "debug.h"
 #else
-#define debug(arg) true
+#define debug(...) 710
 #endif
 
 int main(){
 	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(0);
 }
